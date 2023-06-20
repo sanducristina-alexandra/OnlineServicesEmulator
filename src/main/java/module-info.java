e450c6ae6f2a5941756af432fcdd3bdfdbfafbb3
cristina <sanducristina19@stud.ase.ro>
2023-06-20 12:59:30 +0300

    Created the list of checkboxes for services.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.onlineservicesemulator {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.json;
+
+
+    opens com.example.onlineservicesemulator to javafx.fxml;
+    exports com.example.onlineservicesemulator;
+    exports com.example.classes;
+    opens com.example.classes to javafx.fxml;
+}
