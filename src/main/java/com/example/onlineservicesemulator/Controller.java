01abeb26a99705b4fb02d684fa781187581072d6
Radu <radu1351@yahoo.com>
2023-06-22 16:19:42 +0300

    Created onCheckboxLabelClicked function

--- src/main/java/com/example/onlineservicesemulator/Controller.java
+++ src/main/java/com/example/onlineservicesemulator/Controller.java
@@ -1,59 +1,76 @@
 package com.example.onlineservicesemulator;
 
 import com.example.onlineservicesemulator.classes.JSONReader;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
-    public ListView<String> filesList;
+    private ListView<String> filesList;
     @FXML
     private VBox checkboxList;
-    ArrayList<String> servicesNames;
-    Map<String, List<String>> uploadedFiles;
+    private List<String> servicesNames;
+    private Map<String, List<String>> servicesAndUploadedFilesMap;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         servicesNames = JSONReader.getServices();
+        createAndSetCheckboxList();
         setFilesMap();
-        setCheckboxList();
     }
 
-    public void setCheckboxList() {
-        for (String servicesName : servicesNames) {
-            CheckBox checkBox = new CheckBox(servicesName);
-            checkBox.setId("checkbox_" + servicesName);
+    public void createAndSetCheckboxList() {
+        double lenghtSectionA = calculateLongestServiceString(servicesNames);
+        checkboxList.setPrefWidth(lenghtSectionA);
+        for (String serviceName : servicesNames) {
+            CheckBox checkBox = new CheckBox();
+            checkBox.setId("checkbox_" + serviceName);
+            Label checkBoxLabel = new Label(serviceName);
+            checkBox.setGraphic(checkBoxLabel);
+            checkBoxLabel.setOnMouseClicked(event -> onCheckboxLabelClicked(event, checkBox, checkBoxLabel));
             checkboxList.getChildren().add(checkBox);
             checkboxList.setSpacing(5);
-            checkBox.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent event) {
-                    filesList.getItems().clear();
-
-                    String clickedService = checkBox.getText();
-                    if (uploadedFiles.containsKey(clickedService) && uploadedFiles.get(clickedService) != null) {
-                        filesList.getItems().addAll(uploadedFiles.get(clickedService));
-                    }
-                }
-            });
         }
     }
 
-    public void setFilesMap() {
-        uploadedFiles = new HashMap<>();
-        servicesNames.forEach(service -> uploadedFiles.put(service, null));
+    private void onCheckboxLabelClicked(MouseEvent event, CheckBox checkBox, Label checkBoxLabel) {
+        checkBox.setSelected(false);
+        filesList.getItems().clear();
+        String clickedService = checkBoxLabel.getText();
+        if (servicesAndUploadedFilesMap.containsKey(clickedService) && servicesAndUploadedFilesMap.get(clickedService) != null) {
+            filesList.getItems().addAll(servicesAndUploadedFilesMap.get(clickedService));
+        }
+        event.consume();
+    }
 
-        uploadedFiles.put("WindowControlService", List.of("windowFile1.dat", "windowFile2.dat", "windowFile3.dat"));
-        uploadedFiles.put("MusicControlService", List.of("musicFile1.dat", "musicFile2.dat", "musicFile3.dat"));
+    public void setFilesMap() {
+        servicesAndUploadedFilesMap = new HashMap<>();
+        servicesNames.forEach(service -> servicesAndUploadedFilesMap.put(service, null));
+
+        servicesAndUploadedFilesMap.put("WindowControlService", List.of("windowFile1.dat", "windowFile2.dat", "windowFile3.dat"));
+        servicesAndUploadedFilesMap.put("MusicControlService", List.of("musicFile1.dat", "musicFile2.dat", "musicFile3.dat"));
+    }
+
+    public double calculateLongestServiceString(List<String> servicesNames) {
+        double max = 0;
+        for (String serviceName : servicesNames) {
+            if (serviceName.length() > max) {
+                max = serviceName.length();
+            }
+        }
+        return max * 8;
     }
 
 }
