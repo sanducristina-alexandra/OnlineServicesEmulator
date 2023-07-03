1fa495de2b1de11d8f61dd31d617b387aae2769c
cristina-sandu <sanducristina19@stud.ase.ro>
2023-07-03 10:38:21 +0300

    Updating code

--- src/main/java/com/example/onlineservicesemulator/Controller.java
+++ src/main/java/com/example/onlineservicesemulator/Controller.java
@@ -6,40 +6,62 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
 
     @FXML
+    private ListView<String> filesList;
+    @FXML
     private VBox checkboxList;
-
+    private List<String> servicesNames;
+    private Map<String, List<String>> servicesAndUploadedFilesMap;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        servicesNames = JSONReader.getServices();
         createAndSetCheckboxList();
+        setFilesMap();
     }
 
     public void createAndSetCheckboxList() {
-        List<String> servicesNames = JSONReader.getServices();
-        double lenghtSectionA = calculateLongestServiceString(servicesNames);
-        checkboxList.setPrefWidth(lenghtSectionA);
+        double lengthSectionA = calculateLongestServiceString(servicesNames)*8;
+        checkboxList.setPrefWidth(lengthSectionA);
         for (String serviceName : servicesNames) {
             CheckBox checkBox = new CheckBox();
             checkBox.setId("checkbox_" + serviceName);
             Label checkBoxLabel = new Label(serviceName);
             checkBox.setGraphic(checkBoxLabel);
-            checkBoxLabel.setOnMouseClicked(event -> {
-                checkBox.setSelected(false);
-                event.consume();
-            });
+            checkBoxLabel.setOnMouseClicked(event -> onCheckboxLabelClicked(event, checkBox, checkBoxLabel));
             checkboxList.getChildren().add(checkBox);
             checkboxList.setSpacing(5);
         }
     }
 
+    private void onCheckboxLabelClicked(MouseEvent event, CheckBox checkBox, Label checkBoxLabel) {
+        checkBox.setSelected(false);
+        filesList.getItems().clear();
+        String clickedService = checkBoxLabel.getText();
+        if (servicesAndUploadedFilesMap.containsKey(clickedService) && servicesAndUploadedFilesMap.get(clickedService) != null) {
+            filesList.getItems().addAll(servicesAndUploadedFilesMap.get(clickedService));
+        }
+        event.consume();
+    }
+
+    public void setFilesMap() {
+        servicesAndUploadedFilesMap = new HashMap<>();
+        servicesNames.forEach(service -> servicesAndUploadedFilesMap.put(service, null));
+
+        servicesAndUploadedFilesMap.put("WindowControlService", List.of("windowFile1.dat", "windowFile2.dat", "windowFile3.dat"));
+        servicesAndUploadedFilesMap.put("MusicControlService", List.of("musicFile1.dat", "musicFile2.dat", "musicFile3.dat"));
+    }
     public double calculateLongestServiceString(List<String> servicesNames) {
         double max = 0;
         for (String serviceName : servicesNames) {
