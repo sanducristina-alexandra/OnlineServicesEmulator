ba490d2eb5255703d72cb5d03e5d478e1a01847b
cristina-sandu <sanducristina19@stud.ase.ro>
2023-07-03 10:06:59 +0300

    -

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.onlineservicesemulator {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.json;
 
     opens com.example.onlineservicesemulator to javafx.fxml;
     exports com.example.onlineservicesemulator;
